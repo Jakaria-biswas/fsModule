a037b7661ccdf78049283bf5c29249e40403d90c
contactjakaria <contactjakaria95@gmail.com>
2021-02-16 00:13:03 +0600

    fs write in nond js

--- index.js
+++ index.js
@@ -5,12 +5,31 @@ const http = require('http')
 const server = http.createServer((req, res) => {
        
        if(req.url == '/'){
-           // red file ays 
-             fs.readFile('Home.html', (error, data) => {
-                   res.writeHead(200, {'content-type':'text/html'});
-                   res.write(data);
-                   res.end()
-             })
+
+        //    // red file ays 
+
+        //      fs.readFile('Home.html', (error, data) => {
+        //            res.writeHead(200, {'content-type':'text/html'});
+        //            res.write(data);
+        //            res.end()
+        //      })
+
+          // write file ays 
+
+           fs.writeFile('demo.text','hello world', (error) => {
+                     if(error){
+                                res.writeHead(200, {'content-type' : 'text/html'})
+                                res.write('file write fail');
+                                res.end()
+                     }else{
+                                res.writeHead(200, {'content-type' : 'text/html'})
+                                res.write('file write success');
+                                res.end() 
+                     }
+           })
+        
+
+
        }
 })
 server.listen(3030)
